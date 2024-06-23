67c9ef7b819ae274ed2b5448d46642f8d6d6e5fa
Dmitrii Popov <r0sythedead@yandex.ru>
2024-06-23 18:26:35 +0300

    made a new class Cli and makefile. Added the changes into build.gradle.kts

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,14 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+        Cli cli = new Cli();
+        System.out.println(cli.getRequest());
+        String name = scanner.nextLine();
+        System.out.println(cli.getGreeting() + name + "!");
+
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+public class Cli {
+    private String request = "May I have your name?";
+    private String greeting = "Hello, ";
+
+    public String getRequest(){
+        return request;
+    }
+
+    public String getGreeting(){
+        return greeting;
+    }
+
+
+
+}
